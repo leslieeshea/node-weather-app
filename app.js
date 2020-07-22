9e7ad7faf83570bb1e8ea2cb7451515434ff0c50
Leslie Shea <leslieyshea@gmail.com>
2020-07-21 17:13:42 -0700

    changed unit to farenheit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-const url = 'http://api.weatherstack.com/current?access_key=ade2d7d0f31c35ad7696fc5fc725ca9c&query=37.8267,-122.4233';
+const url = 'http://api.weatherstack.com/current?access_key=ade2d7d0f31c35ad7696fc5fc725ca9c&query=37.8267,-122.4233&units=f';
 
 request({ url: url, json: true }, (error, response) => {
   const currentTemp = response.body.current.temperature;
