1cd43ba23aee348eff23d6cf54044ddb262c9896
Leslie Shea <leslieyshea@gmail.com>
2020-07-21 17:16:07 -0700

    added weather description

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const url = 'http://api.weatherstack.com/current?access_key=ade2d7d0f31c35ad7696
 request({ url: url, json: true }, (error, response) => {
   const currentTemp = response.body.current.temperature;
   const feelsLike = response.body.current.feelslike;
+  const weatherDescription = response.body.current.weather_descriptions[0];
 
-  console.log('It is currently '  + currentTemp + ' degrees out. It feels like ' + feelsLike + ' degrees out.');
+  console.log(weatherDescription + '. It is currently '  + currentTemp + ' degrees out. It feels like ' + feelsLike + ' degrees out.');
   // console.log(response.body.current);
 });
