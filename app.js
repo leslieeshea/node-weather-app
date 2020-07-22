3df3542b3c1abc7b0ecbfe06d8696c77fc32d23d
Leslie Shea <leslieyshea@gmail.com>
2020-07-22 16:29:46 -0700

    added callback chaining to use geocode data in forecast function

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const  geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Los Angeles', (error, data) => {
-  console.log('Error', error);
-  console.log('Data', data);
-});
+geocode('Boston', (error, data) => {
+  if(error) {
+    return console.log(error);
+  }
 
-forecast(34.0544, -118.2439, (error, data) => {
-  console.log('Error', error);
-  console.log('Data', data);
+  forecast(data.latitude, data.longitude, (error, forecastData) => {
+    if(error) {
+      return console.log(error);
+    }
+    
+    console.log(data.location);
+    console.log(forecastData);
+  });
 });
