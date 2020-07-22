b93fa047022b178333653ef7bd3a8225cfed2e36
Leslie Shea <leslieyshea@gmail.com>
2020-07-22 16:32:28 -0700

    able to take in user's location for search

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const  geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Boston', (error, data) => {
-  if(error) {
-    return console.log(error);
-  }
+const location = process.argv[2];
 
-  forecast(data.latitude, data.longitude, (error, forecastData) => {
+if(!location) {
+  console.log('Please provide a location.');
+} else {
+  geocode(location, (error, data) => {
     if(error) {
       return console.log(error);
     }
-    
-    console.log(data.location);
-    console.log(forecastData);
+  
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if(error) {
+        return console.log(error);
+      }
+  
+      console.log(data.location);
+      console.log(forecastData);
+    });
   });
-});
+}
