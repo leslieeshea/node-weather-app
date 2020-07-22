a1d689c3ffc4008ae8d1fe503d407f51c6d8492b
Leslie Shea <leslieyshea@gmail.com>
2020-07-22 16:51:45 -0700

    refactored with es6 object shorthand and destructuring

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,15 +3,15 @@ const request = require('request');
 const forecast = (latitude, longitude, callback) => {
   const url = 'http://api.weatherstack.com/current?access_key=ade2d7d0f31c35ad7696fc5fc725ca9c&query=' + latitude + ',' + longitude + '&units=f';
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if(error) {
       callback('Unable to connect to weather service.');
-    } else if(response.body.error) {
+    } else if(body.error) {
       callback('Unable to find location. Please try again.');
     } else {
-      const currentTemp = response.body.current.temperature;
-      const feelsLike = response.body.current.feelslike;
-      const weatherDescription = response.body.current.weather_descriptions[0];
+      const currentTemp = body.current.temperature;
+      const feelsLike = body.current.feelslike;
+      const weatherDescription = body.current.weather_descriptions[0];
       
       callback(undefined, weatherDescription + '. It is currently '  + currentTemp + ' degrees out. It feels like ' + feelsLike + ' degrees out.');
     }
