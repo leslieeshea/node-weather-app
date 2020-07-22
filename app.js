549db7bc395c8b9238121cf647bc0ceb0de273a0
Leslie Shea <leslieyshea@gmail.com>
2020-07-21 23:36:19 -0700

    added error handling for bad query

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-import request from 'request';
+const request = require('request');
 
-const url = 'http://api.weatherstack.com/current?access_key=ade2d7d0f31c35ad7696fc5fc725ca9c&query=37.8267,-122.4233&units=f';
+const url = 'http://api.weatherstack.com/current?access_key=ade2d7d0f31c35ad7696fc5fc725ca9c&query=37.8267,-122&units=f';
 
 request({ url: url, json: true }, (error, response) => {
-  const currentTemp = response.body.current.temperature;
-  const feelsLike = response.body.current.feelslike;
-  const weatherDescription = response.body.current.weather_descriptions[0];
-
   if(error) {
     console.log('Unable to connect to weather service.');
+  } else if(response.body.error) {
+    console.log('Unable to find location.');
   } else {
+    const currentTemp = response.body.current.temperature;
+    const feelsLike = response.body.current.feelslike;
+    const weatherDescription = response.body.current.weather_descriptions[0];
+
     console.log(weatherDescription + '. It is currently '  + currentTemp + ' degrees out. It feels like ' + feelsLike + ' degrees out.');
   }
 });
